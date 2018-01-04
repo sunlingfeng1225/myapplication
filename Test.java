5da9f963a2b83c80ca37b1c4b240ac44a7226bea
sunlf1225 <sunlf1225@thundersoft.com>
2018-01-04 14:15:21 +0800

    Test.java

--- Test.java
+++ Test.java
@@ -3,5 +3,9 @@ public class Test{
 		System.out.println("welcome to learning Git!")
 		System.out.println("Git 练习");
 		System.out.println("Git 练习.");
+		int a = 1;
+		int b = 2;
+		System.out.println(a+b);
+		System.out.println("test");
 	}
 }
